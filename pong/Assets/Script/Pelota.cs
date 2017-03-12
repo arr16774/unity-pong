ba437084f625f9446a401dff8b491ed69701a919
arr16774 <caarroyavepadilla@gmail.com>
2017-03-12 17:02:23 -0600

    pong
    juego de pong en unity

---
+++ pong/Assets/Script/Pelota.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Carlos Arroyave16774
+
+public class Pelota : MonoBehaviour {
+
+
+	// Use this for initialization
+
+	//se aplica una fuerza al objeto pelota de forma aleatoria. una fuerza vertical y otra horizontal
+	void Start () 
+	{
+		GetComponent<Rigidbody2D>().AddForce(transform.up*250);
+
+		var rand  = Random.Range(1,10);
+		
+		if(rand <= 5)
+		{
+			GetComponent<Rigidbody2D>().AddForce(transform.right*250);
+		}
+		else if(rand>5)
+		{
+			//GetComponent<Rigidbody2D>().AddForce(-1*transform.up*300);
+			GetComponent<Rigidbody2D>().AddForce(-1*transform.right*300);
+		}
+
+
+		
+		
+	}
+	
+	//si existe una colision contra una pared vertical, el objeto "pelota" se destruye
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.name == "ParedIzquierda")
+		{
+			Destroy(this.gameObject);
+		}
+
+		if(col.gameObject.name == "ParedDerecha")
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+}
---
+++ pong/Assets/Script/Raqueta.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Raqueta : MonoBehaviour 
+{
+
+	Vector2 raquetaTras;
+	public int player;
+	float factorVel;
+
+	// Use this for initialization
+	//se inician las condiciones de la raqueta
+	void Start () 
+	{
+		raquetaTras = new Vector2(0.0f,0.0f);
+		factorVel = 20.0f;	
+	}
+	
+	// Update is called once per frame
+
+	//se indica que utilizando las teclas direccionales: arriba y abajo, y las teclas WS, se controla la raqueta para el jugar 1 o jugador 2
+	void Update () 
+	{
+		raquetaTras.y = 0.0f;
+
+
+		//al pulsar R se reinicia la escena del juego
+		if(Input.GetKey(KeyCode.R))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+		if(player == 1)
+		{
+			if(Input.GetKey(KeyCode.UpArrow))
+			{
+				raquetaTras.y = factorVel * Time.deltaTime;
+				transform.Translate(raquetaTras);
+			}
+			if(Input.GetKey(KeyCode.DownArrow))
+			{
+				raquetaTras.y = -1*factorVel * Time.deltaTime;
+				transform.Translate(raquetaTras);
+			}
+		}
+
+		if (player == 2)
+		{
+			if(Input.GetKey(KeyCode.W))
+			{
+				raquetaTras.y = factorVel * Time.deltaTime;
+				transform.Translate(raquetaTras);
+			}
+			if(Input.GetKey(KeyCode.S))
+			{
+				raquetaTras.y = -1*factorVel * Time.deltaTime;
+				transform.Translate(raquetaTras);
+			}
+		}
+	}
+}
+
